dac8c74299361f38b56a2ddc32e46ba52086e712
Lucas <lucasmag97@gmail.com>
2023-12-08 01:21:16 -0300

    refactor key mapping using KeyboardEvent.code as reference

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -4,43 +4,101 @@ import {Howl} from 'howler';
 import {equinoxMappingSet1, SongMappingFactory} from "@src/common/songs-mapping.ts";
 
 export default function Keyboard() {
-  const [keyboardKeys] = React.useState([
-    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-    ['z', 'x', 'c', 'v' ,'b', 'n', 'm', ',', '.', '/', 'Shift', 'Dead']
-  ])
-  const [secondaryKeysMapping] = React.useState<Record<string, string>>({
-    '?': '/',
-    ':': ';',
-    '>': '.',
-    '<': ',',
-    '}': ']',
-    '{': '[',
-    '!': '1',
-    '@': '2',
-    '#': '3',
-    '$': '4',
-    '%': '5',
-    'Dead': '6',
-    '&': '7',
-    '*': '8',
-    '(': '9',
-    ')': '0',
-    '_': '-',
-    '+': '=',
+  // const [keyboardKeys] = React.useState([
+  //   ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
+  //   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']'],
+  //   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter'],
+  //   ['Z', 'X', 'C', 'V' ,'B', 'N', 'M', ',', '.', '/', 'Shift', '']
+  // ])
+
+  const [keyToCode] = React.useState<Record<string, string>>({
+    '1': 'Digit1',
+    '2': 'Digit2',
+    '3': 'Digit3',
+    '4': 'Digit4',
+    '5': 'Digit5',
+    '6': 'Digit6',
+    '7': 'Digit7',
+    '8': 'Digit8',
+    '9': 'Digit9',
+    '0': 'Digit0',
+    '-': 'Minus',
+    '=': 'Equal',
+
+    'Q': 'KeyQ',
+    'W': 'KeyW',
+    'E': 'KeyE',
+    'R': 'KeyR',
+    'T': 'KeyT',
+    'Y': 'KeyY',
+    'U': 'KeyU',
+    'I': 'KeyI',
+    'O': 'KeyO',
+    'P': 'KeyP',
+    '[': 'BracketLeft',
+    ']': 'BracketRight',
+
+    'A': 'KeyA',
+    'S': 'KeyS',
+    'D': 'KeyD',
+    'F': 'KeyF',
+    'G': 'KeyG',
+    'H': 'KeyH',
+    'J': 'KeyJ',
+    'K': 'KeyK',
+    'L': 'KeyL',
+    ';': 'Semicolon',
+    '\'': 'Quote',
+    'Enter': 'Enter',
+
+    'Z': 'KeyZ',
+    'X': 'KeyX',
+    'C': 'KeyC',
+    'V': 'KeyV',
+    'B': 'KeyB',
+    'N': 'KeyN',
+    'M': 'KeyM',
+    ',': 'Comma',
+    '.': 'Period',
+    '/': 'Slash',
+    'Shift': 'ShiftRight',
+    '': 'IntlRo',
   })
+
   const [pressedKeys, setPressedKeys] = React.useState<string[]>([])
   const [equinox, setEquinox] = React.useState<Record<string, Howl>>({})
 
   function playSound(key: string) {
     if (!Object.keys(equinox).includes(key)) {
-      console.log("No mapping for key: ", key)
+      // console.log("No mapping for key: ", key)
       return
     }
 
     equinox[key].play()
   }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    event.preventDefault();
+    console.log(event.keyCode, ' = ', event.code)
+
+    if (event.repeat) return;
+
+    playSound(event.code)
+
+    const key = event.code;
+
+    if (!pressedKeys.includes(key)) {
+      setPressedKeys((prevKeys) => [...prevKeys, key]);
+    }
+  };
+
+  const handleKeyUp = (event: KeyboardEvent) => {
+    event.preventDefault();
+
+    setPressedKeys((prevKeys) => prevKeys.filter((pressedKey) =>
+      pressedKey !== (prevKeys.includes('Shift') && event.code !== 'Shift' ? secondaryKeysMapping[event.code] : event.code)
+    ));
+  };
 
   // console.log(pressedKeys)
 
@@ -52,29 +110,7 @@ export default function Keyboard() {
   }, [])
 
   React.useEffect(() => {
-    console.log("Handlers changed")
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      event.preventDefault();
-      console.log(event)
-
-      if (event.repeat) return;
-
-      playSound(event.key)
-
-      const key = event.key;
-      if (!pressedKeys.includes(key)) {
-        setPressedKeys((prevKeys) => [...prevKeys, key]);
-      }
-    };
-
-    const handleKeyUp = (event: KeyboardEvent) => {
-      event.preventDefault();
-
-      setPressedKeys((prevKeys) => prevKeys.filter((pressedKey) =>
-        pressedKey !== (prevKeys.includes('Shift') && event.key !== 'Shift' ? secondaryKeysMapping[event.key] : event.key)
-      ));
-    };
+    console.log("Reloading")
 
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
@@ -84,17 +120,24 @@ export default function Keyboard() {
       window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('keyup', handleKeyUp);
     };
-  }, [])
+  }, [equinox])
+
 
   return (
     <>
       <div className="keyboard">
-        {keyboardKeys.map((row, rowIndex) =>
+        {Array(4).fill(null).map((_, rowIndex) =>
           <div key={rowIndex} className="keyboard-row">
             {
-              row.map((key, keyIndex) =>
-                <div key={keyIndex} className={`keyboard-key ${pressedKeys.includes(key) ? 'key-pressed' : ''}`}> {key} </div>
-              )
+              Array(12).fill(null).map((_, columnIndex) => {
+                const keyIndex = rowIndex * 12 + columnIndex;
+                const key = Object.keys(keyToCode)[keyIndex];
+                return (
+                  <div key={keyIndex} className={`keyboard-key ${pressedKeys.includes(keyToCode[key]) ? 'key-pressed' : ''}`}>
+                    <b>{key}</b>
+                  </div>
+                )
+              })
             }
           </div>
         )}
