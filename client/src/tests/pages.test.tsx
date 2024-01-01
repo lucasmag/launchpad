9579650f74a57d17bb52731c9726f579979427e5
Lucas <lucasmag97@gmail.com>
2023-12-31 21:35:31 -0300

    add tests

--- client/src/tests/pages.test.tsx
+++ client/src/tests/pages.test.tsx
@@ -1,21 +1,33 @@
 import {act, fireEvent, render, screen} from "@testing-library/react";
 import {expect} from "vitest";
-import App from "@src/App";
+import App, {RoutesContainer} from "@src/App";
 
 import './fetch.mock'
+import {MemoryRouter} from "react-router-dom";
+import {renderWithProviders} from "@src/tests/fetch.mock.tsx";
 
 
 describe('test pages', () => {
-  it('should go back to song selection page when song name is not defined', async () => {
+  it('should allow user to go to play screen', async () => {
     render(<App />)
 
-    await act(async () => {
-      fireEvent.click(await screen.findByRole('button'));
-    })
+    fireEvent.click(await screen.findByRole('button'));
 
     await act(async () => {
       fireEvent.click(await screen.findByText('Skrillex - Kyoto'));
     })
+
+    expect(window.location.pathname).toBe('/play');
+  });
+
+  it('should go back to song selection page when song name is not defined', async () => {
+    renderWithProviders(
+      <MemoryRouter initialEntries={['/play']}>
+        <RoutesContainer />
+      </MemoryRouter>
+    );
+
+    expect(window.location.pathname).toBe('/song-list');
   });
 
   it('should open song list', async () => {
