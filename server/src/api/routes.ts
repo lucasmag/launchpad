0e033bc06607006b38bdf3751a0fc3d87a7f5dd3
Lucas <lucasmag97@gmail.com>
2024-03-18 01:11:09 -0300

    prepare server to aws lambda

---
+++ server/src/api/routes.ts
@@ -0,0 +1,9 @@
+import {getSong, hello} from "@src/api/song";
+import {Router} from "express";
+
+const router = Router();
+
+router.get('/songs/:songName', getSong);
+router.get('/hello', hello);
+
+export default router;
---
+++ server/src/handler.ts
@@ -0,0 +1,5 @@
+import {app} from "@src/index";
+import serverless from 'serverless-http';
+
+// Base AWS Lambda function entrypoint handler
+export const handler = serverless(app);
